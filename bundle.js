5cf9b8dba623039a4d3a87e8db4624447d5e30e4
pankajladhar <ladharpankaj@gmail.com>
2017-03-03 19:40:41 +0530

    Deployed at 2017_03_03__19_40_41

--- bundle.js
+++ bundle.js
@@ -19917,7 +19917,43 @@
 	                return _react2["default"].createElement(
 	                    "div",
 	                    { className: "header" },
-	                    "React Logo Designer"
+	                    "React Logo Designer",
+	                    _react2["default"].createElement(
+	                        "div",
+	                        { className: "socialIcons" },
+	                        _react2["default"].createElement(
+	                            "ul",
+	                            null,
+	                            _react2["default"].createElement(
+	                                "li",
+	                                null,
+	                                _react2["default"].createElement(
+	                                    "a",
+	                                    { href: "https://www.linkedin.com/in/pankaj-ladhar-51781137/", target: "_blank", className: "socialIcons__linkedIn" },
+	                                    _react2["default"].createElement("i", { className: "fa fa-linkedin" }),
+	                                    _react2["default"].createElement(
+	                                        "span",
+	                                        null,
+	                                        "LinkedIn"
+	                                    )
+	                                )
+	                            ),
+	                            _react2["default"].createElement(
+	                                "li",
+	                                null,
+	                                _react2["default"].createElement(
+	                                    "a",
+	                                    { href: "https://github.com/pankajladhar/react-logo-generator", target: "_blank", className: "socialIcons__github" },
+	                                    _react2["default"].createElement("i", { className: "fa fa-github" }),
+	                                    _react2["default"].createElement(
+	                                        "span",
+	                                        null,
+	                                        "Github"
+	                                    )
+	                                )
+	                            )
+	                        )
+	                    )
 	                );
 	            }
 
@@ -20049,7 +20085,7 @@
 	                    { className: "svg-container" },
 	                    _react2["default"].createElement(
 	                        "svg",
-	                        { id: "logo-svg", version: "1.1", xmlns: "http://www.w3.org/2000/svg", x: "0px", y: "0px", width: "600px", height: "600px", viewBox: "0 0 600 600", "enable-background": "new 0 0 600 600" },
+	                        { id: "logo-svg", version: "1.1", xmlns: "http://www.w3.org/2000/svg", x: "0px", y: "0px", viewBox: "0 0 600 600", "enable-background": "new 0 0 600 600" },
 	                        _react2["default"].createElement("rect", { width: "600", height: "600" }),
 	                        _react2["default"].createElement("circle", { cx: "299.529", cy: "299.628", r: "50.167" }),
 	                        _react2["default"].createElement("path", { fill: "none", d: "M299.529,197.628 c67.356,0,129.928,9.665,177.107,25.907c56.844,19.569,91.794,49.233,91.794,76.093c0,27.991-37.041,59.503-98.083,79.728 c-46.151,15.291-106.879,23.272-170.818,23.272c-65.554 0-127.63-7.492-174.29-23.441c-59.046-20.182-94.611-52.103-94.611-79.559 c0-26.642,33.37-56.076,89.415-75.616C167.398,207.503,231.515,197.628,299.529,197.628z" }),
@@ -20057,9 +20093,18 @@
 	                        _react2["default"].createElement("path", { fill: "none", d: "M210.821,351.482 c-33.746-58.292-56.731-117.287-66.312-166.255c-11.544-58.999-3.382-104.109,19.864-117.566 c24.224-14.024,70.055,2.244,118.14,44.94c36.356,32.28,73.688,80.837,105.723,136.173c32.844,56.733,57.461,114.209,67.036,162.582 c12.117,61.213,2.309,107.984-21.453,121.74c-23.057,13.348-65.249-0.784-110.239-39.499 C285.567,460.886,244.898,410.344,210.821,351.482z" })
 	                    ),
 	                    _react2["default"].createElement(
-	                        "a",
-	                        { id: "downloadLink", className: "downloadLink" },
-	                        "Download SVG"
+	                        "div",
+	                        { className: "downloadLinks" },
+	                        _react2["default"].createElement(
+	                            "a",
+	                            { id: "downloadLink", className: "downloadLink button" },
+	                            "Download SVG"
+	                        ),
+	                        _react2["default"].createElement(
+	                            "a",
+	                            { id: "downloadLinkForImage", className: "downloadLink button" },
+	                            "Download Image"
+	                        )
 	                    )
 	                );
 	            }
@@ -37396,7 +37441,7 @@
 	                    ) : null,
 	                    _react2['default'].createElement(
 	                        'button',
-	                        { style: { backgroundColor: options.option2 }, onClick: this.onClickHandler },
+	                        { className: 'button', style: { backgroundColor: options.option2 }, onClick: this.onClickHandler },
 	                        colorName
 	                    )
 	                );
